5a53ff8310cbd2c7bc35cc47acf93e25e61d3ac0
zwir001 <259128@student.pwr.edu.pl>
2021-12-02 11:51:38 +0100

    ruchomy przycisk działa, z "bezpieczną strefą" 20px od lewej

--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -2,15 +2,17 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Window extends JFrame {
 
+     private int position;
+
     public Window( String title ) throws HeadlessException {
+
         super(title);
+        this.position=0;
         build_UI();
     }
     private void build_UI(){
@@ -18,7 +20,7 @@ public class Window extends JFrame {
         JPanel panel= new JPanel();
         panel.setLayout(null);
         panel.setBounds(0, 0 , 650, 460 );
-        add(panel);
+
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -28,30 +30,50 @@ public class Window extends JFrame {
         btn_running.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                super.mouseEntered(e);
-                  run_away(btn_running);
+                    super.mouseEntered(e);
+                    if( need_to_run(e.getX()) )
+                    run_away(btn_running);
             }
         }
 
         );
         panel.add(btn_running);
         JButton reset=new JButton("Reset");
-        reset.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setDefault(reset, btn_running, panel);
-            }
-        });
+        reset.addActionListener(e -> setDefault(reset, btn_running));
         panel.add(reset);
-        setDefault(reset, btn_running, panel);
+        setDefault(reset, btn_running);
+        getContentPane().add(panel);
     }
-    private void setDefault( JButton reset, JButton run, JPanel panel)
+    private void setDefault( JButton reset, JButton run)
     {
         run.setBounds(220, 220, 100, 45);
         reset.setBounds(500, 300, 100,45);
     }
+
+    private boolean need_to_run( int x){
+        return x >= 20 && x < 100;
+    }
+
     private void run_away(JButton run)
     {
-        run.setBounds(100, 199, 100, 45);
+        switch (this.position) {
+            case 0 -> {
+                run.setBounds(100, 199, 100, 45);
+                this.position = 1;
+            }
+            case 1 -> {
+                run.setBounds(50, 130, 100, 45);
+                this.position = 2;
+            }
+            case 2 -> {
+                run.setBounds(400, 80, 100, 45);
+                this.position = 3;
+            }
+            case 3 -> {
+                run.setBounds(200, 250, 100, 45);
+                this.position = 0;
+            }
+            default -> run.setBounds(220, 220, 100, 45);
+        }
     }
 }
